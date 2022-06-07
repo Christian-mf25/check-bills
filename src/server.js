7e2d436f3c2bcff026ceb059075210e20fd5d48a
Christian <christian.mf22@gmail.com>
2022-06-07 10:30:09 -0300

    feat: README.md written

--- src/server.js
+++ src/server.js
@@ -6,6 +6,4 @@ dotenv.config();
 
 const port = process.env.PORT ? process.env.PORT : 8080;
 
-app.listen(port, () => {
-  console.log(`App running on port http://localhost:${port}`);
-});
+app.listen(port, () => {});
